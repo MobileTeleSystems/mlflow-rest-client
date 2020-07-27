e41fe49f58273f9318eafcd52bbbf956644c820a
Maxim Martynov <msmarty5@mts.ru>
2020-07-27 14:51:47 +0300

    [SCRR-111] Add samples

--- samples/sample.py
+++ samples/sample.py
@@ -5,9 +5,6 @@ from mlflow_client.log import get_logger
 
 logger = get_logger()
 
-def now():
-    return int(time.time())
-
 def process(client):
     logger.info("====== list_experiments")
     exps = client.list_experiments()
@@ -15,60 +12,54 @@ def process(client):
     for exp in exps:
         logger.info("  {}".format(exp))
 
-    logger.info("====== create_experiment")
-    experiment_name = "py_exp_"+str(time.time()).replace(".","")
-    logger.info("CreateExperiment with name {}".format(experiment_name))
-    experiment_id = client.create_experiment(experiment_name)
-    logger.info("create_experiment: rsp.experiment_id: {}",experiment_id)
+    logger.info("====== get_or_create_experiment")
+    experiment_name = "py_exp_"+str(time.time()).replace(".", "")
+    logger.info("create experiment with name {}".format(experiment_name))
+    experiment = client.get_or_create_experiment(experiment_name)
+    logger.info("  id: {}".format(experiment.id))
 
     logger.info("====== create_run")
     run_name = 'run_for_exp_' + experiment_name
-    current_file = os.path.basename(__file__)
-    start_time = now()
-    dct = {'experiment_id': experiment_id, 'run_name': run_name, 'source_type': 'LOCAL', 'source_name': current_file, 'start_time': start_time }
-    rsp = client.create_run(dct)
-    logger.info("create_run rsp:",rsp)
-    run_uuid = rsp['run_uuid']
+    start_time = client._now
+    run = client.create_run(experiment_id=experiment.id, name=run_name, start_time=start_time)
+    logger.info("  run id {}".format(run.id))
 
-    logger.info("====== Log params and metrics")
+    logger.info("====== log_run_parameter and metrics")
     param_key = 'max_depth'
-    metric_key = 'auc'
-    client.log_parameter(run_uuid, param_key, '2')
-    client.log_metric(run_uuid, metric_key, .99)
+    param_value = '2'
+    client.log_run_parameter(run.id, param_key, param_value)
 
-    logger.info("====== update_run Run")
-    dct = {'run_uuid': run_uuid, 'status': 'FINISHED', 'end_time': start_time+1000 }
-    logger.info("update_run req:",dct)
-    client.update_run(dct)
+    logger.info("====== log_run_metric")
+    metric_key = 'auc'
+    metric_value = .59
+    client.log_run_metric(run.id, metric_key, metric_value)
+    metric_value = .69
+    client.log_run_metric(run.id, metric_key, metric_value, step=1, timestamp=start_time+10)
+    metric_value = .79
+    client.log_run_metric(run.id, metric_key, metric_value, step=2, timestamp=start_time+15)
+    metric_value = .89
+    client.log_run_metric(run.id, metric_key, metric_value, step=2, timestamp=start_time+20)
+    metric_value = .99
+    client.log_run_metric(run.id, metric_key, metric_value, step=3, timestamp=start_time+30)
+
+    logger.info("====== finish_run")
+    client.finish_run(run.id, end_time=start_time+20)
 
     logger.info("====== get_run")
-    run = client.get_run(run_uuid)
-    logger.info("get_run rsp:",run)
+    run = client.get_run(run.id)
+    logger.info("  {}".format(run))
 
     logger.info("====== get_experiment")
-    exp = client.get_experiment(experiment_id)
-    logger.info("get_experiment rsp:",exp)
-
-    logger.info("====== get_metric")
-    rsp = client.get_metric(run_uuid, metric_key)
-    logger.info("get_metric rsp:",rsp)
+    experiment = client.get_experiment(experiment.id)
+    logger.info("  {}".format(experiment))
 
     logger.info("====== get_metric_history")
-    rsp = client.get_metric_history(run_uuid, metric_key)
-    logger.info("get_metric_history rsp:",rsp)
+    metric_history = client.get_run_metric_history(run.id, metric_key)
+    logger.info("  {}".format(metric_history))
 
-    logger.info("====== list_artifacts")
-    path = ''
-    rsp = client.list_artifacts(run_uuid, path)
-    logger.info("list_artifacts rsp:",rsp)
-
-    logger.info("====== Search")
-    expIds = [ experiment_id]
-    clauses = [
-        { 'type': 'parameter', 'comparator': '=', 'key': param_key, 'value': '2'},
-        { 'type': 'metric', 'comparator': '>=', 'key': metric_key, 'value': .99} ]
-    rsp = client.search2(expIds,clauses)
-    logger.info("search2 rsp:",rsp)
+    logger.info("====== list_run_artifacts")
+    artifacts = client.list_run_artifacts(run.id)
+    logger.info("  {}".format(artifacts))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
