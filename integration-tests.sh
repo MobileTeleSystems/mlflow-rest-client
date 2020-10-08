4819c7fbd565fb38033092c239fe0a45a881d522
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2020-10-08 18:52:32 +0300

    [DSX-45] Add pytest_runner.sh

--- integration-tests.sh
+++ integration-tests.sh
@@ -3,4 +3,4 @@
 wait-for-it.sh -h $MLFLOW_HOST -p $MLFLOW_PORT
 
 python_version=$(python -c 'import sys; print("{0}.{1}".format(*sys.version_info))')
-coverage run -m pytest tests/integration --junitxml=./reports/junit/integration-${python_version}.xml --reruns 5 $@
+pytest_runner.sh tests/integration --junitxml=./reports/junit/integration-${python_version}.xml --reruns 5 $@
