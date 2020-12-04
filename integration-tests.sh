73742f52b2bb948da36346fd8f3abcfca8e30f1e
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2020-12-04 14:04:02 +0300

    [DSX-34] Fix integrational tests

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wait-for-it.sh -h $MLFLOW_HOST -p $MLFLOW_PORT
+wait-for-it.sh -h $MLFLOW_HOST -p $MLFLOW_PORT -t 0
 
 python_version=$(python -c 'import sys; print("{0}.{1}".format(*sys.version_info))')
 pytest_runner.sh tests/integration --junitxml=./reports/junit/integration-${python_version}.xml --reruns 5 $*
