ebc26637147cd928bf326f8819cac26d4bf49521
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2021-03-12 19:02:36 +0300

    [DSX-166] Move version detection before root imports

--- mlflow_client/__init__.py
+++ mlflow_client/__init__.py
@@ -1,10 +1,12 @@
+# pylint: disable= wrong-import-position
+
 from __future__ import absolute_import
 
 import os
-
-from mlflow_client.mlflow_api_client import MLflowApiClient
 
 VERSION_FILE = os.path.join(os.path.dirname(__file__), "VERSION")
 
 with open(VERSION_FILE) as f:
     __version__ = f.read().strip()
+
+from .mlflow_api_client import MLflowApiClient
